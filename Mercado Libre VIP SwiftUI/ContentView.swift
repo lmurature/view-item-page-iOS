9a37ea6c513444ff8f929d41891bfd3d75111017
lmurature <lucio.murature@mercadolibre.com>
2020-10-19 17:25:57 -0300

    Initial Commit

---
+++ Mercado Libre VIP SwiftUI/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Mercado Libre VIP SwiftUI
+//
+//  Created by Lucio Murature on 19/10/2020.
+//  Copyright © 2020 Lucio Murature. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
